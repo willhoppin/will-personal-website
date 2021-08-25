598781f24ce296af11655a490223ef327b147a50
Will Hoppin <whoppin@middlebury.edu>
2021-08-25 16:50:26 -0700

    add new "more like this" section

--- src/app/feature-pages/coding/cheese/cheese.component.ts
+++ src/app/feature-pages/coding/cheese/cheese.component.ts
@@ -7,6 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CheeseComponent implements OnInit {
 
+  related = [
+    {
+      image: '/assets/features/mb-code.png',
+      link: '/cheese'
+    },
+    {
+      image: '/assets/features/mb-com-code.png',
+      link: '/cheese'
+    },
+    {
+      image: '/assets/features/pi-code.png',
+      link: '/cheese'
+    },
+    {
+      image: '/assets/features/willflix-code.png',
+      link: '/cheese'
+    },
+    {
+      image: '/assets/features/catan-code.png',
+      link: '/cheese'
+    }
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
