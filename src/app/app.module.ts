33680264ffd3f09f9474de62392a10e4778c7977
Will Hoppin <whoppin@middlebury.edu>
2021-08-25 11:21:17 -0700

    nav component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { FeatureComponent } from './components/feature/feature.component';
 import { CheeseComponent } from './feature-pages/coding/cheese/cheese.component';
+import { NavComponent } from './components/nav/nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     FeatureComponent,
-    CheeseComponent
+    CheeseComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
