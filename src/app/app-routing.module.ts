1f2fe7f836ec032817f2b1b15ca6f24d6cc86414
Will Hoppin <whoppin@middlebury.edu>
2023-06-01 11:29:40 -0400

    cheese

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CatanComponent } from './feature-pages/coding/catan/catan.component';
 import { DarpaComponent } from './feature-pages/coding/darpa/darpa.component';
+import { NbcuComponent } from './feature-pages/coding/nbcu/nbcu.component';
+import { A24Component } from './feature-pages/coding/a24/a24.component';
 import { CheeseComponent } from './feature-pages/coding/cheese/cheese.component';
 import { MagicBinderComComponent } from './feature-pages/coding/magic-binder-com/magic-binder-com.component';
 import { MagicBinderComponent } from './feature-pages/coding/magic-binder/magic-binder.component';
@@ -14,6 +16,8 @@ const routes: Routes = [
   { path: 'cheese', component: CheeseComponent },
   { path: 'catan', component: CatanComponent },
   { path: 'darpa', component: DarpaComponent },
+  { path: 'nbcu', component: NbcuComponent },
+  { path: 'a24', component: A24Component },
   { path: 'magic-binder', component: MagicBinderComponent },
   { path: 'magic-binder-com', component: MagicBinderComComponent },
   { path: 'prod-ind', component: ProdIndComponent },
--- src/app/feature-pages/coding/a24/a24.component.ts
+++ src/app/feature-pages/coding/a24/a24.component.ts
@@ -7,6 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class A24Component implements OnInit {
 
+  related = [
+    {
+      image: '/assets/features/mb-code.png',
+      link: '/magic-binder'
+    },
+    {
+      image: '/assets/features/mb-com-code.png',
+      link: '/magic-binder-com'
+    },
+    {
+      image: '/assets/features/pi-code.png',
+      link: '/prod-ind'
+    },
+    {
+      image: '/assets/features/willflix-code.png',
+      link: '/willflix'
+    },
+    {
+      image: '/assets/features/catan-code.png',
+      link: '/catan'
+    }
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
